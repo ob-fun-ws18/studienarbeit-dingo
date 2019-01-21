0b2003ba30d41d287377e8679e3d3c41e9dfdac9
Chris Brammer <c.brammer1@gmail.com>
2019-01-21 13:18:06 +0100

    Catch exception on EOF (Windows...)

--- src/P2PChat/Socket.hs
+++ src/P2PChat/Socket.hs
@@ -3,11 +3,18 @@ module P2PChat.Socket (
 ) where
 
 import System.IO
+import Control.Exception
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as BL
 
-readHandle :: Handle -> IO (Maybe BL.ByteString)
-readHandle handle = do
+readHandle :: Handle -> IO (Maybe BL.ByteString) 
+readHandle handle = readHandle' handle `catch` handler
+  where
+    handler :: SomeException -> IO (Maybe BL.ByteString) 
+    handler ex = return Nothing
+
+readHandle' :: Handle -> IO (Maybe BL.ByteString)
+readHandle' handle = do
   eof <- hIsEOF handle
   if eof then
     return Nothing
